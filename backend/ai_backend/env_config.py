87fb9cb69d16385ca3e65840447b2b8364ca4940
suyambu-raja <asuyamburaja35@gmail.com>
2026-04-06 21:49:57 +0530

    Removed sensitive config file

--- backend/ai_backend/env_config.py
+++
@@ -1,7 +0,0 @@
-# Environment variables configuration for development
-# This file is ignored by git to keep secrets safe.
-
-import os
-
-os.environ['GEMINI_API_KEY'] = 'AIzaSyC9R9MFFyB1bA3bOuxPQOQ8WlFaWngCVfs'
-os.environ['GEMINI_API_KEY_BACKUP'] = 'AIzaSyDigba0xso9FmuQpX_ljlLfmEwXFVKBnhU'
